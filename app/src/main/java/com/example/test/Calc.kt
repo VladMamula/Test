4025ddbe5c847677e8bec211b8a731e9175004fe
VladMamula <vlad_mamula@mail.ru>
2021-10-07 22:30:58 +0300

    Result and Design Help Activity

--- app/src/main/java/com/example/test/Calc.kt
+++ app/src/main/java/com/example/test/Calc.kt
@@ -3,16 +3,34 @@ package com.example.test
 import kotlin.random.Random
 
 class Calc {
-    var a= arrayOfNulls<Int>(5)
-    var b= arrayOfNulls<Int>(5)
-    var oper:Array<String> = arrayOf("+","-","*","/","+")
+    var a= arrayOfNulls<Int>(5) // массив первых чисел примеров
+    var b= arrayOfNulls<Int>(5) // массив вторых чисел примеров
+    var oper:Array<String> = arrayOf("+","-","*","/","+") //массив математ. операций
 
     init {
-        for (i in 0..4) {
+        for (i in 0..4) { // заполняем массивы рандомными числами от -99 до 99
             a[i] = Random.nextInt(-99, 99)
             b[i] = Random.nextInt(-99,99)
-            oper.shuffle()
+            oper.shuffle() // перемешиваем мат. операции
         }
+    }
+
+    fun result(a:Int?, b: Int?, str:String): Int { // функция для возвращения ответа примера
+        when (str) {
+            "+" -> if (a != null && b != null) { // проверяем значения чтобы они были не null
+                return a+ b                      // возвращаем результат и так со всеми операциями
+            }
+            "-" -> if (a != null && b != null ) {
+                return a-b
+            }
+            "*" -> if (a != null && b != null) {
+                return a*b
+            }
+            "/" -> if (a != null && b != null) {
+                return a/b
+            }
+        }
+        return 0
     }
 
 }
--- app/src/main/java/com/example/test/MainActivity.kt
+++ app/src/main/java/com/example/test/MainActivity.kt
@@ -7,25 +7,41 @@ import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
 
-    var calc: Calc = Calc()
+    var calc: Calc = Calc() // создаем экземпляр класса Calc
+    var rating: Int = 0 // оценка
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        generate()
+        generate() //вызываем функцию generate
+        result.setOnClickListener { result() } // обрабатывем клик на кнопку Результат
     }
 
-    private fun generate() {
-        for (i in 0..4) {
+    private fun generate() { // функция которая генерирует примеры
+        for (i in 0..4) {  // проверяем знак, если деление, то подгоняем первое число под второе, чтобы ответ был целочисленным и в промежутке от -10 до 10
             if (calc.oper[i] == "/")
                 calc.a[i] = calc.b[i]?.times(Random.nextInt(-10, 10))
         }
-            tv1.text = "1) (${calc.a[0].toString()}) ${calc.oper[0]} (${calc.b[0]})"
-            tv2.text = "2) (${calc.a[1].toString()}) ${calc.oper[1]} (${calc.b[1]})"
-            tv3.text = "3) (${calc.a[2].toString()}) ${calc.oper[2]} (${calc.b[2]})"
-            tv4.text = "4) (${calc.a[3].toString()}) ${calc.oper[3]} (${calc.b[3]})"
-            tv5.text = "5) (${calc.a[4].toString()}) ${calc.oper[4]} (${calc.b[4]})"
+        tv1.text = "1) (${calc.a[0].toString()}) ${calc.oper[0]} (${calc.b[0]})" //выводим на экран примеры
+        tv2.text = "2) (${calc.a[1].toString()}) ${calc.oper[1]} (${calc.b[1]})"
+        tv3.text = "3) (${calc.a[2].toString()}) ${calc.oper[2]} (${calc.b[2]})"
+        tv4.text = "4) (${calc.a[3].toString()}) ${calc.oper[3]} (${calc.b[3]})"
+        tv5.text = "5) (${calc.a[4].toString()}) ${calc.oper[4]} (${calc.b[4]})"
     }
 
+
+    private fun result() { // функция, которая подсчитывает оценку
+        rating = 0 // обнуляем оценку при каждом вызове
+        val d = arrayOfNulls<String>(5)   // массив правильных ответов
+        for (i in 0..4) {
+            d[i] = calc.result(calc.a[i], calc.b[i], calc.oper[i]).toString()  // заполняем массив
+        }
+        if (et1.text.toString() == d[0])  rating++  //считываем данные с editText, проверяем правильность и подсчитываем оценку
+        if (et2.text.toString() == d[1])  rating++
+        if (et3.text.toString() == d[2])  rating++
+        if (et4.text.toString() == d[3])  rating++
+        if (et5.text.toString() == d[4])  rating++
+        tv_result.text = "Оценка: $rating"  //выводим оценку на экран
+    }
 
 }
